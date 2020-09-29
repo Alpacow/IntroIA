125766a2609ff483be2bab2702aa723294aa5d42
Alpacow <fvpereira@inf.ufsm.br>
2020-09-28 21:25:24 -0300

    ia burra fodas

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -18,8 +18,10 @@ def initial_state():
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
 
+
 def count_empty(board):
     return len([e for listL in board for e in listL if e is None])
+    
 
 # Returns player who has the next turn on a board. 
 def player(board):
@@ -31,19 +33,7 @@ def player(board):
             return O
         else:
             return X
-            
-# Returns set of all possible actions (i, j) available on the board.
-"""
-def actions(board):
-    possibles = []
-    for i in range (0, 3):
-        for j in range(0, 3):
-            aux = copy.deepcopy(board)
-            if board[i][j] == None:
-                aux[i][j] = ROUND
-                possibles.append(aux)
-    return possibles
-"""
+
 
 def get_letter(board):
     return O if count_empty(board) % 2 == 0 else X
@@ -58,14 +48,12 @@ def actions(board):
 
 # Returns the board that results from making move (i, j) on the board.
 def result(board, action):
-    board[action[0]][action[1]] = get_letter(board)
-    return board
-
-# Returns the board that results from making move (i, j) on the board.
-def resultAux(board, action):
-    board[action[1][0]][action[1][1]] = get_letter(board)
-    return board
-
+    aux = copy.deepcopy(board)
+    if action[0] >= 0 and action[0] <=2 and action[1] >= 0 and action[1] <= 2:
+        aux[action[0]][action[1]] = get_letter(board)
+        return aux
+    else:
+        raise Exception("Index out of range")
 
 # Returns the winner of the game, if there is one.
 def winner(board):
@@ -80,7 +68,7 @@ def winner(board):
 
 # Returns True if game is over, False otherwise.
 def terminal(board):
-    return False if utility(board) == 0 else True
+    return False if winner(board) == None and count_empty(board) != 0 else True
 
 
 # Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
@@ -90,18 +78,19 @@ def utility(board):
         diag2 = []
         aux = 2 # aux for secondary diagonal
         result = 1 if t == X else -1
-        for i in range (0, 3):
+        for i in range (0, 3): # check elements per line
             if all(e == t for e in board[i]):
                 return result
             col = []
             diag = []
             for j in range (0, 3):
                 col.append(board[j][i])
-                diag.append(board[i][j])
             if all(e == t for e in col): # check elements per column
                 return result
             diag2.append(board[i][aux])
             aux = aux - 1
+        for i in range(0, 3):
+            diag.append(board[i][i])
         if all(e == t for e in diag): # check main diagonal
                 return result
         if all(e == t for e in diag2): # check secondary diagonal5
@@ -109,31 +98,44 @@ def utility(board):
     return 0
 
 def maxvalue(board, v):
+    #aux = copy.deepcopy(board)
     if terminal(board):
         return [utility(board), (v[1])]
     for a in actions(board):
         v[1] = a
-        r = minvalue(resultAux(board, v), v)
+        r = minvalue(result(board, a), v)
         v[0] = max(v[0], r[0])
     return v
 
 def minvalue(board, v):
+    #aux = copy.deepcopy(board)
     if terminal(board):
         return [utility(board), (v[1])]
     for a in actions(board):
         v[1] = a
-        r = maxvalue(resultAux(board, v), v)
+        r = maxvalue(result(board, a), v)
         v[0] = min(v[0], r[0])
     return v
 
 # Returns the optimal action for the current player on the board.
 def minimax(board):
+    print("\n*************************************************** ENTROU MINMAX *************************************\n")
+    if terminal(board):
+        return None
     if get_letter(board) == X:
-        v = [-2, (0,0)]
+        v = [-math.inf, (0,0)]
         r = maxvalue(board, v)
+        print("X maximizou para ", r)
         return r[1] # X aims to maximize score
     else:
-        v = [2, (0,0)]
+        v = [math.inf, (0,0)]
         r = minvalue(board, v)
-        print(r)
+        print("O minimizou para ", r)
         return r[1] # O aims to minimize
+
+"""
+        if r[0] >= 2: # 2 is beta value
+            return v
+        if r[0] > v[0]:
+            v[0] = r[0]
+"""
